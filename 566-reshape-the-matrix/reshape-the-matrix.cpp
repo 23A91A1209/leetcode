786cf76be880556981352bb23324b2a46d54064c
Mounika <147414489+23A91A1209@users.noreply.github.com>
2025-12-18 14:50:37 +0530

    Time: 2 ms (22.20%) | Memory: 16.3 MB (6.91%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        vector<vector<int>>res(r,vector<int>(c));
+        vector<int>arr;
+        int m=c*r;
+        int ct=0;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                arr.push_back(mat[i][j]);
+                ct++;
+            }
+        }
+        if(ct!=m){
+            return mat;
+        }
+        int k=0;
+        while(k<arr.size()){
+            for(int i=0;i<r;i++){
+                for(int j=0;j<c;j++){
+                    res[i][j]=arr[k];
+                    k++;
+                }
+            }
+        }
+        return res;
+
+    }
+};
