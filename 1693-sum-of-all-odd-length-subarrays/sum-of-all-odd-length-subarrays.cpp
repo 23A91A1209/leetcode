b0fb69ca87adf38583f9d27c09f471077331be47
Mounika <147414489+23A91A1209@users.noreply.github.com>
2025-12-23 15:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (18.91%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        vector<int>prefix(arr.size());
-        prefix[0]=arr[0];
-        for(int i=1;i<arr.size();i++){
-            prefix[i]=prefix[i-1]+arr[i];
-        }
-        int totalsum=0;
-        for(int i=0;i<arr.size();i++){
-            for(int j=i;j<arr.size();j++){
-                int sum=0;
-                if((j-i+1)%2!=0){
-                    if(i==0){
-                    sum+=prefix[j];
-                     }
-                     else{
-                        sum+=prefix[j]-prefix[i-1];
-                     }
-                   
-                }
-                
-                totalsum+=sum;
-            }
+           int n=arr.size();
+           int totalsum=0;
+           for(int i=0;i<arr.size();i++){
+              int r;
+              if(i==0) r=arr.size()-i;
+              else r=(i+1)*(arr.size()-i);
+              totalsum+=ceil(r/2.0)*arr[i];
+
+           }
             
+           
+           return totalsum;
+        
         }
-        return totalsum;
-    }
 };
