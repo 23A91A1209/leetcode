29573209f599171bb033323c095a14340da86a46
Mounika <147414489+23A91A1209@users.noreply.github.com>
2025-12-29 20:23:26 +0530

    Time: 3 ms (15.83%) | Memory: 10.6 MB (7.43%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        vector<int>prefix(n+1,0);
+   
+        for(int i=1;i<=n;i++){
+            prefix[i]=prefix[i-1]+i;
+        }
+        
+        for(int i=1;i<=n;i++){
+            int left=prefix[i];
+            int right=prefix[n]-prefix[i-1];
+            if(left==right){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+};
