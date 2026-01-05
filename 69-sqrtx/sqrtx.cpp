6943eef6b4156920c78e9e0e37112e6d877ce665
Mounika <147414489+23A91A1209@users.noreply.github.com>
2026-01-05 20:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.90%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int n=150;
+        long long i=-1;
+        long long j=x;
+        if(x==0) return 0;
+        if(x==1) return 1;
+        while(n--){
+            long long mid=(i+j)/2;
+            if(mid*mid<=x){
+                i=mid;
+            }
+            else{
+                j=mid;
+            }
+        }
+        return i;
     }
 };
