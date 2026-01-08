3864a3e5d7e45812b355708af8d38b0c56bca397
Mounika <147414489+23A91A1209@users.noreply.github.com>
2026-01-08 15:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (23.99%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
@@ -12,30 +12,35 @@
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<vector<int>> ans;
+        vector<double> ans;
         queue<TreeNode*>q;
         if(root!=NULL) q.push(root);
         while(!q.empty()){
             int s=q.size();
             vector<int>res;
+            long long sum=0;
             for(int i=0;i<s;i++){
+
                 res.push_back(q.front()->val);
+                sum+=q.front()->val;
                 if(q.front()->left!=NULL) q.push(q.front()->left);
                 if(q.front()->right!=NULL) q.push(q.front()->right);
+                
                 q.pop();
             }
-            ans.push_back(res);
+            ans.push_back((double)sum/s);
         }
-        vector<double>result;
-        for(int i=0;i<ans.size();i++){
-            vector<int>h=ans[i];
-            long long sum=0;
-            for(int j=0;j<h.size();j++){
-                cout<<h[j]<<" "<<endl;
-                sum+=h[j];
-            }
-             result.push_back((double)sum/h.size());
-        }
-        return result;
+        // vector<double>result;
+        // for(int i=0;i<ans.size();i++){
+        //     vector<int>h=ans[i];
+        //     long long sum=0;
+        //     for(int j=0;j<h.size();j++){
+        //         cout<<h[j]<<" "<<endl;
+        //         sum+=h[j];
+        //     }
+        //      result.push_back((double)sum/h.size());
+        // }
+        // return result;
+        return ans;
     }
 };
