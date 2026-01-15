091a5773b7b6b6239b25a2845e050b712f11a531
Mounika <147414489+23A91A1209@users.noreply.github.com>
2026-01-15 11:51:24 +0530

    Time: 3 ms (40.08%) | Memory: 10 MB (64.97%) - LeetSync

---
+++ 4186-count-residue-prefixes/count-residue-prefixes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        int i=0,j=0,c=0;
+        set<char>st;
+        for(int j=0;j<s.size();j++){
+           st.insert(s[j]);
+
+            int distinct=st.size();
+            if(distinct==(j+1)%3){
+                c++;
+            }
+            
+        }
+        return c;
+    }
+};
