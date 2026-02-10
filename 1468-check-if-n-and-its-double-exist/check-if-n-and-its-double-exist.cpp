2c458c2c34c4aa9b52d4a1b1d933bf87bf6eb0d6
Mounika <147414489+23A91A1209@users.noreply.github.com>
2026-02-10 19:31:46 +0530

    Time: 3 ms (28.96%) | Memory: 13.6 MB (51.41%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        bool isthere=false;
+        for(int i=0;i<arr.size();i++){
+            for(int j=i+1;j<arr.size();j++){
+                if(arr[i]==2*arr[j] || arr[j]==2*arr[i]){
+                    isthere=true;
+                }
+            }
+        }
+        return isthere;
+    }
+};
