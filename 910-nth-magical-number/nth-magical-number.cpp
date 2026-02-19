7cba82d5fd41b8660ed9da03932aa3437b8ba629
Mounika <147414489+23A91A1209@users.noreply.github.com>
2026-02-19 16:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (42.87%) - LeetSync

---
+++ 910-nth-magical-number/nth-magical-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        long long l=min(a,b);
+        long long  h=1ll*n*min(a,b);
+        long long mod=1e9+7;
+        long long  ans=0;
+        while(l<=h){
+            long long mid=(l+h)/2;
+            long long dvda=mid/a;
+             long long dvdb=mid/b;
+             long long lcm=a*b/__gcd(a,b);
+             long long dvdboth=mid/lcm;
+             long long count=dvda+dvdb-dvdboth;
+            if((mid%a==0 || mid%b==0)&& count==n) return mid%mod;
+            else if(n<=count){
+                ans=mid;
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
